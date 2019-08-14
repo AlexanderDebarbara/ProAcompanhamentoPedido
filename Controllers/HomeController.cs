59756853385fed9999a7a3602a703df13422e44c
Alexander <debarbara4@gmail.com>
2019-08-13 21:47:49 -0400

    Finished

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAcompanhamentoPedido.Models;
+using ProAcompanhamentoPedido.Repository;
 
 namespace ProAcompanhamentoPedido.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AcompanhamentoContexto _context;
+
+        public HomeController(AcompanhamentoContexto context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Pedido> pedidos = await _context.Pedidos.Include(p => p.Cliente).ToListAsync();
+            return View(pedidos);
         }
     }
 }
